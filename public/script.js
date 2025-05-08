75da19394dcf60be78094f306048c7f5e96c1bd5
quatroxx <serhatha@gmail.com>
2025-05-08 19:56:34 +0300

    Update script.js

--- public/script.js
+++ public/script.js
@@ -210,9 +210,9 @@ fetch("/products.json")
     <div class="info-top">
       <div></div> <!-- soldaki boşluk için -->
       <div class="favorite-icon" onclick="toggleFavorite(this)">
-        <svg class="heart" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="#333" stroke-width="1.5">
-          <path d="M12 21s-6.4-4.1-9-9c-1.6-3.2 0.4-7 4.3-7 2.1 0 3.7 1.4 4.7 2.8C13 6.4 14.6 5 16.7 5c3.9 0 5.9 3.8 4.3 7-2.6 4.9-9 9-9 9z"/>
-        </svg>
+        <svg class="heart" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="#333" stroke-width="1.4" stroke-linecap="round" stroke-linejoin="round">
+    <path d="M20.8 4.6c-1.5-1.4-4-1.4-5.5 0l-1.3 1.3-1.3-1.3c-1.5-1.4-4-1.4-5.5 0s-1.5 3.7 0 5.1l6.8 6.7 6.8-6.7c1.5-1.4 1.5-3.7 0-5.1z"/>
+  </svg>
       </div>
     </div>
     <h3>${product.title}</h3>
